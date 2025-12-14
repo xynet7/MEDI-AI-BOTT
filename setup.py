ee2c78a83ca461d5ce2fb9515503a6a98f9943b0
Nayan Mitra <103122689+xynet7@users.noreply.github.com>
2025-12-14 13:39:53 +0530

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Nayan Mitra",
+    author_email="vmitra844@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
